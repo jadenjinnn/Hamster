334d562df1ade3c741355e6d5643b09f616d85ad
DoritoThePug <jadenjin12345@gmail.com>
2026-05-11 15:20:43 +0100

    fix: expose Transform rotation and size as readwrite properties in Python binding

--- Hamster-Py/src/Components.h
+++ Hamster-Py/src/Components.h
@@ -7,5 +7,7 @@ namespace py = pybind11;
 void TransformBinding(py::module_ &m) {
   py::class_<Hamster::Transform>(m, "Transform")
       .def(py::init<glm::vec3, float, glm::vec3>())
-      .def_readwrite("position", &Hamster::Transform::position);
+      .def_readwrite("position", &Hamster::Transform::position)
+      .def_readwrite("rotation", &Hamster::Transform::rotation)
+      .def_readwrite("size", &Hamster::Transform::size);
 }
